f346115c3224c97c64b73605a2efddf65983e628
lastunicorn <lastunicorn@997bf151-6ffc-4b03-94d9-ab1c1f146a20>
2012-07-15 10:26:57 +0000

    Added the CommandLine dll to the project.

---
+++ SharpKinoko/SharpKinoko.Console/CommandLineArguments.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine;
+
+namespace DustInTheWind.SharpKinoko.SharpKinokoConsole
+{
+    class CommandLineArguments
+    {
+        [Option("a", "assemly", Required = false, HelpText = "A list of assembly file names.")]
+        public string AssemblyFileNames { get; set; }
+    }
+}
--- SharpKinoko/SharpKinoko.Tests/Console/CommandArguments/ArgumentsTests/IndexerTests.cs
+++ SharpKinoko/SharpKinoko.Tests/Console/CommandArguments/ArgumentsTests/IndexerTests.cs
@@ -9,7 +9,7 @@ namespace DustInTheWind.SharpKinoko.Tests.Console.CommandArguments.ArgumentsTest
         [Test]
         public void argument_has_null_value_if_only_name_provided_in_the_string()
         {
-            string[] args = new string[] { "-a" };
+            string[] args = new[] { "-a" };
             Arguments arguments = new Arguments(args);
 
             Assert.That(arguments["a"], Is.Null);
@@ -18,10 +18,19 @@ namespace DustInTheWind.SharpKinoko.Tests.Console.CommandArguments.ArgumentsTest
         [Test]
         public void argument_has_value_if_is_provided_in_the_string()
         {
-            string[] args = new string[] { "-a", "val1" };
+            string[] args = new[] { "-a", "val1" };
             Arguments arguments = new Arguments(args);
 
             Assert.That(arguments["a"], Is.EqualTo("val1"));
+        }
+
+        [Test]
+        public void argument_has_null_value_if_next_item_is_another_argument()
+        {
+            string[] args = new[] { "-a", "-b" };
+            Arguments arguments = new Arguments(args);
+
+            Assert.That(arguments["a"], Is.Null);
         }
     }
 }
